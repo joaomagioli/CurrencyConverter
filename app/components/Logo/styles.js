c2727fd9b0cbc3d31a06345300aecb967944aed0
Joao Guilherme <joaomagioli@gmail.com>
2018-12-06 14:34:16 -0200

    Adding image background

---
+++ app/components/Logo/styles.js
@@ -0,0 +1,28 @@
+import EStyleSheet from 'react-native-extended-stylesheet';
+import { Dimensions } from 'react-native';
+
+const imageWidth = Dimensions.get('window').width / 2;
+
+const styles = EStyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
+    containerImage: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: imageWidth,
+        height: imageWidth,
+    },
+    logo: {
+        width: imageWidth / 2,
+    },
+    text: {
+        color: '$white',
+        fontSize: 28,
+        letterSpacing: -0.5,
+        marginTop: 15,
+        fontWeight: '600',
+    },
+});
+
+export default styles;
