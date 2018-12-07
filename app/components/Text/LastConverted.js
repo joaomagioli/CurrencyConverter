16eeee2a3eda77dc998ede1e11458b4e690ed126
Joao Guilherme <joaomagioli@gmail.com>
2018-12-07 17:14:35 -0200

    Styled text added

---
+++ app/components/Text/LastConverted.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { Text } from 'react-native'
+import styles from './styles'
+import moment from 'moment'
+
+export default class LastConverted extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <Text style={styles.smallText}>
+                1 {this.props.base} = {this.props.conversionRate}
+                {this.props.quote}
+                as of {moment(this.props.date).format('MMMM D, YYYY')}
+            </Text>
+        )
+    }
+}
+
+LastConverted.propTypes = {
+    date: PropTypes.object,
+    base: PropTypes.string,
+    quote: PropTypes.string,
+    conversionRate: PropTypes.number
+}
