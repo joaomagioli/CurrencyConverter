9509ee0e791a67b25a49ae5bab3416c9abc0586e
Joao Guilherme <joaomagioli@gmail.com>
2018-12-19 14:11:48 -0200

    Adding actions for currency list

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,5 +1,7 @@
-export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT';
-export const SWAP_CURRENCY = 'SWAP_CURRENCY';
+export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT'
+export const SWAP_CURRENCY = 'SWAP_CURRENCY'
+export const CHANGE_BASE_CURRENCY = 'CHANGE_BASE_CURRENCY'
+export const CHANGE_QUOTE_CURRENCY = 'CHANGE_QUOTE_CURRENCY'
 
 export const changeCurrencyAmount = amount => ({
     type: CHANGE_CURRENCY_AMOUNT,
@@ -9,3 +11,13 @@ export const changeCurrencyAmount = amount => ({
 export const swapCurrency = () => ({
     type: SWAP_CURRENCY,
 });
+
+export const changeBaseCurrency = (baseCurrency) => ({
+    type: CHANGE_BASE_CURRENCY,
+    baseCurrency,
+})
+
+export const changeQuoteCurrency = (quoteCurrency) => ({
+    type: CHANGE_QUOTE_CURRENCY,
+    quoteCurrency,
+})
--- app/screens/CurrencyList.js
+++ app/screens/CurrencyList.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import { FlatList, View, StatusBar } from 'react-native'
 import { ListItem, Separator } from '../components/List'
 import currencies from '../data/currencies'
+import { changeBaseCurrency, changeQuoteCurrency } from '../actions/currencies'
+import { connect } from 'react-redux'
 
 const TEMP_CURRENT_CURRENCY = 'CAD'
 
-export default class CurrencyList extends Component {
+class CurrencyList extends Component {
 
-    handlePress = () => {
-        console.log('row press')
+    static propTypes = {
+        navigation: PropTypes.object,
+        dispatch: PropTypes.func
+    }
+
+    handlePress = (currency) => {
+        const { type } = this.props.navigation.this.state.params
+
+        if (type === 'base') {
+            this.props.dispatch(changeBaseCurrency(currency))
+        } else if (type === 'quote') {
+            this.props.dispatch(changeQuoteCurrency)
+        }
+
+        this.props.navigation.goBack(null)
     }
 
     render() {
@@ -20,10 +36,12 @@ export default class CurrencyList extends Component {
                     renderItem={({ item }) => (
                         <ListItem text={item}
                             selected={item === TEMP_CURRENT_CURRENCY}
-                            onPress={this.handlePress} />)}
+                            onPress={(item) => this.handlePress(item)} />)}
                     keyExtractor={(item) => item}
                     ItemSeparatorComponent={Separator} />
             </View >
         )
     }
 }
+
+export default connect()(CurrencyList)
